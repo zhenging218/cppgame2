adc2de93fac13ee5d0d88717d95e910d1a9ab034
Yiu Zhen Ging <zhenging218@gmail.com>
2026-02-18 13:45:16 +0800

    fix determinant incorrect

--- engine/src/maths/math_helper.cpp
+++ engine/src/maths/math_helper.cpp
@@ -3,16 +3,16 @@
 namespace cppengine {
     namespace MathHelper {
         float determinant2D(const float &v,
-     const float &a, const float &b,
-     const float &c, const float &d) {
+             const float &a, const float &b,
+             const float &c, const float &d) {
             return v * (a * d - c * b);
         }
 
         float determinant3D(const float &v,
-                const float& a, const float& b, const float &c,
-                const float& d, const float& e, const float &f,
-                const float &g, const float &h, const float &i) {
-            return v * determinant2D(a, e, f, g, i) - determinant2D(b, d, f, g, i) + determinant2D(c, d, e, g, h);
+            const float& a, const float& b, const float &c,
+            const float& d, const float& e, const float &f,
+            const float &g, const float &h, const float &i) {
+            return v * (a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g));
         }
     }
 
