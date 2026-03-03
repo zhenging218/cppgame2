b614fa7bd6ea178b5f1f3344f82aaf0632630a0b
Yiu <zhenging.yiu@dxc.com>
2026-03-03 10:36:22 +0800

    create postbuild script to copy/symlink the assets folder

---
+++ game/game_postbuild.cmake
@@ -0,0 +1,29 @@
+
+if(NOT EXISTS "${ASSET_SYMLINK_LOCATION}")
+    message(STATUS "Creating symlink [${ASSET_DIR} <-> ${ASSET_SYMLINK_LOCATION}]")
+    execute_process(
+            COMMAND ${CMAKE_COMMAND} -E create_symlink
+            "${ASSET_DIR}"
+            "${ASSET_SYMLINK_LOCATION}"
+            RESULT_VARIABLE result
+            ERROR_QUIET
+    )
+
+    if(NOT result EQUAL 0)
+        message(WARNING "failed to create symlink [${ASSET_DIR} <-> ${ASSET_SYMLINK_LOCATION}], will perform a copy")
+        execute_process(
+                COMMAND ${CMAKE_COMMAND} -E copy_directory
+                "${ASSET_DIR}"
+                "${ASSET_SYMLINK_LOCATION}"
+                RESULT_VARIABLE result
+                ERROR_QUIET
+        )
+        if(NOT RESULT EQUAL 0)
+            message(WARNING "failed to copy [${ASSET_DIR} <-> ${ASSET_SYMLINK_LOCATION}]...")
+        endif()
+    else()
+        message(STATUS "symlink [${ASSET_DIR} <-> ${ASSET_SYMLINK_LOCATION}] created successfully")
+    endif()
+else()
+    message(STATUS "symlink [${ASSET_DIR} <-> ${ASSET_SYMLINK_LOCATION}] already exists, skipping...")
+endif()
